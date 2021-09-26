4b00bdf6a454881f8908b01d4deb141db93e1f5e
junlovecat <74006534+junlovecat@users.noreply.github.com>
2021-09-26 16:10:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,95 +1,112 @@
-print('Pyviewer v1.2 made by JUN')
-from keyboard import is_pressed
-import pygame
+print('Pyviewer v1.3 made by JUN')
+from threading import current_thread
 import time
 import sys
+try:
+    from keyboard import is_pressed
+    import pygame
+except ModuleNotFoundError:
+    import os
+    os.system('pip install keyboard')
+    os.system('pip install pygame')
+    from keyboard import is_pressed
+    import pygame
 sys.stdin=open('stdin.txt')
-isp=input().split(' ')
-n=len(isp)
-blit=input().split(' ')
+line=int(input())
+keys=[0 for i in range(line)]
+n=0
+linemax=-987654321
+for i in range(line):
+    keys[i]=list(map(str,input().split()))
+    n+=len(keys[i])
+    if(linemax<len(keys[i])):
+        linemax=len(keys[i])
+blit=[[0 for j in range(len(keys[i]))] for i in range(len(keys))]
+for i in range(len(keys)):
+    for j in range(len(keys[i])):
+        if(keys[i][j]=='space'):
+            blit[i][j]=' '
+        else:
+            blit[i][j]=keys[i][j].upper()
 showkey,shownum,showtot,showcur,showavg=map(int,input().split())
 tmp=0
 max=0
 sec=0
 avg=0
-total=[0 for i in range(n)]
-ispre=[0 for i in range(n)]
+total=[[0 for j in range(len(keys[i]))] for i in range(len(keys))]
+cur_list=[[0 for j in range(len(keys[i]))] for i in range(len(keys))]
+ispre=[[0 for j in range(len(keys[i]))] for i in range(len(keys))]
 got=[]
 black=(0,0,0)
 white=(255,255,255)
 gray=(128,128,128)
-if(showavg+showcur+showtot==0):
-    size=[100*n+5,100]
+if(showavg+showcur+showtot+showcur,showavg==0):
+    size=[100*linemax+5,100*(line+1)]
 else:
-    size=[100*n+5,200]
+    size=[100*linemax+5,100*(line+2)]
 pygame.init()
-pygame.display.set_caption("Pyviewer v1.2")
-screen= pygame.display.set_mode(size)
-clock=pygame.time.Clock()
-font = pygame.font.SysFont(None,30)
-got.append(sum(total))
-ispressing=[0 for i in range(0,n)]
+pygame.display.set_caption("Pyviewer v1.3(beta)")
+screen=pygame.display.set_mode(size)
+font=pygame.font.SysFont(None,30)
 ischecked=True
-nowcur=[0]
 stt=time.time()
+tot=0
+starter=time.time()
+ischecked=False
 while(1):
-    #for CUR
-    if(ischecked):
-        starter=time.time()
-        ischecked=False
-    #show key
-    for x in isp:
-        if(is_pressed(x)):
-            pygame.draw.rect(screen,white,[isp.index(x)*100+5,5,90,90])
-            if(ispressing[isp.index(x)]==0):
-                ispressing[isp.index(x)]=1
-                total[isp.index(x)]+=1
-                tmp+=1
-        if not is_pressed(x):
-            pygame.draw.rect(screen,gray,[isp.index(x)*100+5,5,90,90])
-            ispressing[isp.index(x)]=0
-    if(showtot):
-        pygame.draw.rect(screen, gray, [5,105,90,90])
-    if(showkey):
-        for x in blit:
-            a=font.render(x,True,black)
-            screen.blit(a,(43+100*(blit.index(x)),45))
-    #show NUM, TOT, CUR, AVG
-    if(shownum):
-        for x in range(len(total)):
-            a=font.render(str(total[x]),True,black)
-            screen.blit(a,(5+100*x,65))
-    if(showtot):
-        totl = font.render("TOT",True,black)
-        screen.blit(totl,(30,135))
-        sumtotl = font.render(str(sum(total)),True,black)
-        screen.blit(sumtotl,(30,160))
-    now=time.time()-starter
-    if(now>=1):
-        if(showavg):
-            pygame.draw.rect(screen, gray, [205,105,90,90])
-        if(showcur):
-            pygame.draw.rect(screen, gray, [105,105,90,90])
-        if(tmp>max):
-            max=tmp
-        if(showcur):
-            curt = font.render("CUR",True,black)
-            screen.blit(curt,(130,135))
-            cur = font.render(str(tmp),True,black)
-            screen.blit(cur,(130,160))
-        if(tmp!=0):
-            avg = (avg * sec + tmp) / (sec + 1)
-            n+=1
-        if(showavg):
-            avgt= font.render("AVG",True,black)
-            screen.blit(avgt,(230,135))
-            avgg = font.render(str(int(sum(total)/(time.time()-stt))),True,black)
-            screen.blit(avgg,(230,160))
-        prev=tmp
-        tmp=0
-        ischecked=True
-    
-    #TRUE MAIN
+    for linenum in range(0,line):
+        for x in range(0,len(keys[linenum])):
+            if(is_pressed(keys[linenum][x])):
+                pygame.draw.rect(screen,white,[x*100+5,linenum*100+5,90,90])
+                if(ispre[linenum][x]==0):
+                    ispre[linenum][x]=1
+                    total[linenum][x]+=1
+                    tmp+=1
+                    tot+=1
+        for x in range(0,len(keys[linenum])):
+            if not is_pressed(keys[linenum][x]):
+                pygame.draw.rect(screen,gray,[x*100+5,linenum*100+5,90,90])
+                ispre[linenum][x]=0
+        for x in range(0,len(keys[linenum])):
+            if(showtot):
+                pygame.draw.rect(screen, gray, [5,205,90,90])
+            if(showkey):
+                a=font.render(str(blit[linenum][x]),True,black)
+                screen.blit(a,(43+100*x,100*linenum+45))
+            if(shownum):
+                a=font.render(str(total[linenum][x]),True,black)
+                screen.blit(a,(5+100*x,linenum*100+65))
+            if(showtot):
+                totl = font.render("TOT",True,black)
+                screen.blit(totl,(30,235))
+                sumtotl = font.render(str(tot),True,black)
+                screen.blit(sumtotl,(30,260))
+        now=time.time()-starter
+        if(now>=1):
+            sec=time.time()-stt
+            if(showavg):
+                pygame.draw.rect(screen, gray, [205,205,90,90])
+            if(showcur):
+                pygame.draw.rect(screen, gray, [105,205,90,90])
+            if(tmp>max):
+                max=tmp
+            if(showcur):
+                curt = font.render("CUR",True,black)
+                screen.blit(curt,(130,235))
+                cur = font.render(str(tmp),True,black)
+                screen.blit(cur,(130,260))
+            if(tmp!=0):
+                avg=(avg*sec+tmp)/(sec+1)
+                n+=1
+            if(showavg):
+                avgt= font.render("AVG",True,black)
+                screen.blit(avgt,(230,235))
+                avgg = font.render(str(int(avg)),True,black)
+                screen.blit(avgg,(230,260))
+            prev=tmp
+            tmp=0
+            starter=time.time()
+            continue
     pygame.display.update()
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
